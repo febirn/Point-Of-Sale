cf0cb25ee1fa592a0c6bb45d162a8436244328ff
febirn <febirn8@gmail.com>
2017-03-16 14:04:12 +0700

    Tambah Tabel User

---
+++ db/migrations/20170316034127_create_table_user.php
@@ -0,0 +1,40 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class CreateTableUser extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+        $user = $this->table('users');
+        $user->addColumn('username', 'string')
+             ->addColumn('password', 'string')
+             ->addColumn('name', 'string')
+             ->addColumn('status', 'integer')
+             ->addColumn('update', 'timestamp')
+             ->addColumn('created', 'timestamp', ['default' => 'CURRENT_TIMESTAMP'])
+             ->addColumn('deleted', 'integer', ['default' => 0])
+             ->create();
+    }
+}
